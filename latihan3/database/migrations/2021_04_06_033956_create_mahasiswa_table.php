f7ffe57e14a568da89c54cf65d622eb38f1f9a97
zavira111 <agnesoctavianda123@gmail.com>
2021-04-06 14:37:11 +0700

    mahasiswa

---
+++ latihan3/database/migrations/2021_04_06_033956_create_mahasiswa_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->bigIncrements('nim', 12);
+            $table->timestamps();
+            $table->varchar('nama', 20);
+            $table->varchar('alamat', 30);
+            $table->number('nomor_tlp', 14);
+            $table->date('ttl');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
